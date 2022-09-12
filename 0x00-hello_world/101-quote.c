8ab0462784b47bf3ca5894eb786d5812a8b89ebe
nazaek <chinazaekperejustice@gmail.com>
2022-09-11 23:11:11 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
  * main - Entry point
@@ -8,7 +7,7 @@
  */
 int main(void)
 {
-	char ch[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, ch, sizeof(ch));
 	return (0);
