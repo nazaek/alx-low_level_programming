76a30643b6bacb45f2356d22a9b27db910883249
nazaek <chinazaekperejustice@gmail.com>
2022-09-11 21:57:33 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-entry: prints the console.
+ * Return:0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
