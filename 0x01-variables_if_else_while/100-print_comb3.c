224c284a1a282e4e8d1a7bfc437a2908a86639f3
nazaek <chinazaekperejustice>
2022-09-12 22:48:08 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,19 +10,23 @@
 int main(void)
 {
 		int d, j;
-
+		
 		for (d = 0; d <= 8; d++)
-
-		for (j = 1; j <= 9; j++)
 		{
-			putchar(d + '0');
-			putchar(j + '0');
+			for (j = 1; j <= 9; j++)
+			{
+				if (d < j)
+				{
+					putchar(d + '0');
+					putchar(j + '0');
+					if (d != 8 || j != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
 		}
-			if (d != 8 || j != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-			putchar('\n');
-			return (0);
+		putchar('\n');
+		return (0);
 }
