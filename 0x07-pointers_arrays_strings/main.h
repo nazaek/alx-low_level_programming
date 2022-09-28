c4e44b2e841d0e19a7140fc8bdfc073c18ad3166
nazaek <chinazaekperejustice>
2022-09-27 20:18:54 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,21 +2,19 @@
 #define MAIN_H
 
 /**
- * File: main.h
- * Description: all functions used in the 0x06-pointer assignment
+ * void prototypes ()
+ * int prototypes ()
  */
 
-#include <stddef.h>
-
-int_putchar(char c):
-void *_memset(void *s, int c, size_t n);
+int _putchar(char);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-unsigned int _strspn(char * s, char c *accept);
+unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif
+#endif /*MAIN_H*/
