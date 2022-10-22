54c04d2040773066d90023fd13841f52eab67da2
nazaek <chinazaekperejustice@gmail.com>
2022-10-21 21:53:01 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,10 +9,10 @@
 int len(const char *str)
 {
 	int count;
-if (str == NULL)
+
+	if (str == NULL)
 		return (0);
-	for
- (count = 0; str[count] != '\0'; count++)
+	for (count = 0; str[count] != '\0'; count++)
 		;
 	return (count);
 }
@@ -25,3 +25,27 @@ if (str == NULL)
  *
  * Return: the address of the new element, or NULL if it failed.
  */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = (list_t *) malloc(sizeof(list_t));
+	list_t *last = *head;
+
+	if (new_node == NULL || str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	new_node->len = len(str);
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new_node;
+	return (*head);
+}
