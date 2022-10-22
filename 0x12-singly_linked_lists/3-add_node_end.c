ca6dfc7bc266842e4a49a63ce334bed6328f73ab
nazaek <chinazaekperejustice@gmail.com>
2022-10-21 21:47:45 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,9 +1,27 @@
-(45 sloc)  975 Bytes
-
 #include "lists.h"
 
 /**
  * len - Calculates the length of a string.
  * @str: Constant string
  * Return: The length of the string
+ */
+
+int len(const char *str)
+{
+	int count;
+if (str == NULL)
+		return (0);
+	for
+ (count = 0; str[count] != '\0'; count++)
+		;
+	return (count);
+}
+
+/**
+ * add_node_end - Function that adds a new node at the end of a list_t list.
+ *
+ * @head: This is the input linked list
+ * @str: This is the string take in my linked list.
+ *
+ * Return: the address of the new element, or NULL if it failed.
  */
